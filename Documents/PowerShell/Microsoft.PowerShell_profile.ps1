28229a67937ec06e179f7d8695e73cc3dda3bea5
Shynur <one.last.kiss@outlook.com>
2023-12-20 19:01:49 +0800

    Add files via upload

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+Set-PSReadLineOption -EditMode Emacs
+. ~/.emacs.d/etc/use-emacs.ps1 d:/Progs/emacs/
+
+# 1_shell kushal sim-web clean-detailed di4m0nd emodipt-extend night-owl negligible
+oh-my-posh init pwsh  `
+  --config ~/AppData/Local/Programs/oh-my-posh/themes/jandedobbeleer.omp.json   `
+  | Invoke-Expression
