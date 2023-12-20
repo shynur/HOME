40f94f7bcf3b472c28beb6e7db98a23d55c76485
Shynur <one.last.kiss@outlook.com>
2023-12-20 19:41:22 +0800

    Add files via upload

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,9 @@
 Set-PSReadLineOption -EditMode Emacs
 . ~/.emacs.d/etc/use-emacs.ps1 d:/Progs/emacs/
 
-# 1_shell kushal sim-web clean-detailed di4m0nd emodipt-extend night-owl negligible
-oh-my-posh init pwsh  `
-  --config ~/AppData/Local/Programs/oh-my-posh/themes/jandedobbeleer.omp.json   `
-  | Invoke-Expression
+if (-not $env:EMACS_INVOKED_PWSH) {
+    # 1_shell kushal sim-web clean-detailed di4m0nd emodipt-extend night-owl negligible
+    oh-my-posh init pwsh  `
+      --config ~/AppData/Local/Programs/oh-my-posh/themes/1_shell.omp.json   `
+      | Invoke-Expression
+}
