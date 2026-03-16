b63c6f0d78d6eb06b4a866e48f12fb0f1015b225
𝓈𝒽𝓎𝓃𝓊𝓇 <shynur@outlook.com>
2026-03-16 02:43:27 +0000

    ;

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,1 +1,14 @@
 ;; -*- lexical-binding: t; -*-
+
+(require 'package)
+(setopt package-archives
+        '(("gnu"    . "https://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+          ("nongnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+          ("melpa-stable" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/stable-melpa/")))
+(package-initialize)
+
+(setopt custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+(add-hook 'kill-emacs-query-functions
+          #'custom-prompt-customize-unsaved-options)
