a86638a0a92f9ca6327e219c778834215f37be82
谢骐 <shynur@outlook.com>
2026-07-10 13:17:46 +0800

    Guard PSReadLine settings in non-interactive sessions

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,6 @@
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
+if (-not [Console]::IsInputRedirected -and -not [Console]::IsOutputRedirected) {
+    Set-PSReadLineOption -EditMode Emacs
+    Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+    Set-PSReadLineOption -PredictionViewStyle ListView
+    Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
+}
