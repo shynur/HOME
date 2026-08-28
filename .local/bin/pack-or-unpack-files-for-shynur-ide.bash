c2ea03e0cd8c0a5e3c920ebb39b287bfa7551d84
𝓈𝒽𝓎𝓃𝓊𝓇 <shynur@outlook.com>
2026-08-28 21:47:18 +0800

    Create pack-or-unpack-files-for-shynur-ide.bash

---
+++ .local/bin/pack-or-unpack-files-for-shynur-ide.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Usage: 如果提供参数, 则 $1 视为存储了文件的 base64 的环境变量, 脚本将其解压; 否则压缩文件并输出 base64.
+
+cd `mktemp -d`
+
+compressed_files_filename=files.tar.gz
+
+if [ $1 ]; then
+    base64 <<<${!1} -d >$compressed_files_filename
+    tar -P -xzvf $compressed_files_filename
+else
+    tar -P -I 'gzip -9' -cvf $compressed_files_filename -T <(
+        cat <<-'EOF'
+		/etc/shynur-ide/
+		/root/.git-credentials
+		/root/.bash_history
+	EOF
+    )
+    base64 files.tar.gz
+fi
