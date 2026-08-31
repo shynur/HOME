3842a000148c98f953b54a6bb8c004d5e2495373
𝓈𝒽𝓎𝓃𝓊𝓇 <shynur@outlook.com>
2026-08-31 11:39:24 +0800

    Update init-cnb-kimi-sessions.bash

--- .local/bin/init-cnb-kimi-sessions.bash
+++ .local/bin/init-cnb-kimi-sessions.bash
@@ -6,6 +6,6 @@ if ! gh auth token &>/dev/null; then
     gh auth login
 fi
 
-gh repo clone shynur/kimi-sessions
+gh repo clone shynur/kimi-sessions -- --depth=1
 cd kimi-sessions
 mv -f -- * .*  ~/.kimi-code
