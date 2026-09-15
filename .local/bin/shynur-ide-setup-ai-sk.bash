d7a5b318d1f108f2b04e967b5612ef1ed39d641a
𝓈𝒽𝓎𝓃𝓊𝓇 <shynur@outlook.com>
2026-09-15 23:04:33 +0800

    Update shynur-ide-setup-ai-sk.bash

--- .local/bin/shynur-ide-setup-ai-sk.bash
+++ .local/bin/shynur-ide-setup-ai-sk.bash
@@ -3,13 +3,13 @@
 . /etc/shynur-ide/ai-sk.sh
 
 if [ "$AI_ALIBABA_BAILIAN" ]; then
-    if ! grep -Pzq '(?m)^[[:blank:]]*\[providers\.seer-openai\][[:blank:]]*\napi_key[[:blank:]]*=' ~/.kimi-code/config.toml; then
-        sed -i '/^[[:blank:]]*\[providers\.seer-openai][[:blank:]]*$/a api_key="'"$AI_ALIBABA_BAILIAN"\" ~/.kimi-code/config.toml
+    if ! grep -Pzq '(?m)^[[:blank:]]*\[providers\.alibaba-cn\][[:blank:]]*\napi_key[[:blank:]]*=' ~/.kimi-code/config.toml; then
+        sed -i '/^[[:blank:]]*\[providers\.alibaba-cn\][[:blank:]]*$/a api_key="'"$AI_ALIBABA_BAILIAN"\" ~/.kimi-code/config.toml
     fi
 fi
 
 if [ "$AI_SEER" ]; then
-    if ! grep -Pzq '(?m)^[[:blank:]]*\[providers\.alibaba-cn\][[:blank:]]*\napi_key[[:blank:]]*=' ~/.kimi-code/config.toml; then
-        sed -i '/^[[:blank:]]*\[providers\.alibaba-cn][[:blank:]]*$/a api_key="'"$AI_SEER"\" ~/.kimi-code/config.toml
+    if ! grep -Pzq '(?m)^[[:blank:]]*\[providers\.seer-openai\][[:blank:]]*\napi_key[[:blank:]]*=' ~/.kimi-code/config.toml; then
+        sed -i '/^[[:blank:]]*\[providers\.seer-openai\][[:blank:]]*$/a api_key="'"$AI_SEER"\" ~/.kimi-code/config.toml
     fi
 fi
