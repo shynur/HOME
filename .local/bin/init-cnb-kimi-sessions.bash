f0fc82d6c090240c4ca2f07b36c6d827b1b88c6a
𝓈𝒽𝓎𝓃𝓊𝓇 <shynur@outlook.com>
2026-09-20 10:32:06 +0800

    Update init-cnb-kimi-sessions.bash

--- .local/bin/init-cnb-kimi-sessions.bash
+++ .local/bin/init-cnb-kimi-sessions.bash
@@ -17,5 +17,6 @@ else
     until which rsync &>/dev/null; do
         sleep 1
     done
-    rsync -avz --delete ./ root@172.17.0.1:/home/shynur/.kimi-code/
+    rsync -avz --delete .git/ root@172.17.0.1:/home/shynur/.kimi-code/.git/
+    ssh root@172.17.0.1 'cd ~shynur/.kimi-code; git reset --hard HEAD'
 fi
